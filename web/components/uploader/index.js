b8070b83b6291d1331b7a40195aaf5dee5e6b2db
Yanbing Dong <dongyanbing@metasota.ai>
2024-03-21 12:46:30 +0800

    uploader

--- web/components/uploader/index.js
+++ web/components/uploader/index.js
@@ -1,7 +1,34 @@
-export default () => {
+import { useDropzone } from "react-dropzone";
+
+export default ({
+  onChange,
+}) => {
+  const onDrop = (acceptedFiles) => {
+    acceptedFiles.forEach((acceptedFile) => {
+      onChange({
+        source: acceptedFile,
+        image: URL.createObjectURL(acceptedFile),
+      });
+    });
+  };
+  const { getRootProps, getInputProps, open } = useDropzone({
+    accept: {
+      'image/jpeg': [ '.jpg', '.jpeg' ],
+      'image/png': [],
+      'image/webp': [],
+    },
+    onDrop,
+    multiple: false,
+  });
+
   return (
-    <div className='flex items-center justify-center mt-2 border-2 rounded h-16'>
-      Upload Image
+    <div className='flex items-center justify-center mt-2 border-2 rounded cursor-pointer p-2' {...getRootProps()} >
+      <input {...getInputProps()} />
+      <img src="/image-placeholder.svg" alt="placeholder" className='h-12 rounded-lg'/>
+      <div className='text-xs text-center ml-2'>
+        Upload an image <br />
+        (.jpg, .png, .webp)
+      </div>
     </div>
   );
 }
