53e3ba285569b2f5f8907b6226f7798db4ccb54b
Yanbing Dong <dongyanbing@metasota.ai>
2024-03-23 17:44:12 +0800

    compressing loading

---
+++ web/components/compressLoading/index.js
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <div className='flex items-center justify-center mt-8'>
+      <div>
+        <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-gray-600 mx-auto" ></div>
+        <p>{"Compressing Image..."}</p>
+      </div> 
+    </div>
+  );
+};
