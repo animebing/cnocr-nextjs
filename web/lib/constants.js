0e79bd7c396b7c986127fd36dd69983b06c15841
animebing <yanbing_dong@hotmail.com>
2024-03-24 20:20:09 +0800

    update

--- web/lib/constants.js
+++ web/lib/constants.js
@@ -2,5 +2,5 @@ export const imageTypes = [
   {'value': 'densenet_lite_136-gru', 'label': 'General Image'},
   {'value': 'scene-densenet_lite_136-gru', 'label': 'Scene Image'},
   {'value': 'doc-densenet_lite_136-gru', 'label': 'Doc Image'},
-  {'value': 'number-densenet_lite_136-gru', 'label': 'Number Image'},
+  {'value': 'number-densenet_lite_136-fc', 'label': 'Number Image'},
 ]
